eab4d3917e31cb8515bd25c53b1489a89a52e320
riochristine <riochristineb@gmail.com>
2024-10-30 11:21:36 +0800

    Add notes on JavaScript: Functions.

---
+++ 6 - Javascript/05-functions.js
@@ -0,0 +1,17 @@
+// Functions
+function greet() {
+  console.log("Hello, welcome to the world of function");
+}
+
+// Calling function
+greet();
+
+// Function with parameters
+// parameters - placeholder
+// Arguments
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log("Sum:", sum(5, 10));
+

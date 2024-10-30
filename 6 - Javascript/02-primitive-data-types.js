da4e4c405969e0c8260ea8381b5f33c3b982a466
riochristine <riochristineb@gmail.com>
2024-10-30 11:17:11 +0800

    Add notes on JavaScript: Data Types.

---
+++ 6 - Javascript/02-primitive-data-types.js
@@ -0,0 +1,28 @@
+// Data types
+
+// Primitive Data Types
+
+// Strings - textual data enclosed in '' or ""
+let firstName = "John";
+console.log("Type of Firstname:", typeof firstName);
+
+// Number -represent a numeric value (e.g. 30, -3, 3.14)
+let age = 30;
+console.log(age);
+console.log("Type of age:", typeof age);
+
+// Boolean - Either true or false
+let isStudent = true;
+console.log(isStudent);
+console.log("Type of isStudent:", typeof isStudent);
+
+// Null - Intentional absence of value
+let car = null;
+console.log(car);
+console.log("Type of car:", typeof car);
+
+// Undefined - Unintentiional absence of value
+// let city;
+let city = undefined;
+console.log(city);
+console.log("Type of city:", typeof city);
